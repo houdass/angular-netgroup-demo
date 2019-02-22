72ba76f9f862fbc788145510dbeb768d8619e575
Youness Houdass <youness.h@hiitconsulting.com>
2019-02-22 12:16:19 +0000

    1.Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskInfoComponent } from './task/task-info/task-info.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskListComponent,
+    TaskInfoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
